f6b405c248bfcbadc61cbc443184ab4f2bb3c323
SubhasishDutta-Coder <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-03-25 21:47:49 +0600

    Add files via upload

---
+++ A is unchangable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{int a,b,c;
+printf("remember that here A is unchangeable\n");
+printf("Enter the velocity\n");
+scanf("%d",&a);
+printf("enter the time\n");
+scanf("%d",&b);
+c=a*2*b;
+printf("the passes distance is %d\n",c);
+return 0;
+}
+
